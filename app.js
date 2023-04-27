7004b6df4d58ad48b377ee2bb2ffcee7b68f26e0
minato <tigerrabbit22@outlook.com>
2023-04-27 02:55:58 -0400

    afternoon

--- app.js
+++ app.js
@@ -8,12 +8,7 @@ const gptConfiguration = new Configuration({
 const openai = new OpenAIApi(gptConfiguration)
 const app = express();
 
-
-
-
-const sampleAnswer1 = " I am sad because my husband left me. It’s stressing me out because I don’t have enough money.";
 async function validateOneSentence( answer) {
-    //const prompt = "'" + answer + "'" + "\n" + "If above answer has only one sentence, please write '1'. If not, please write '2'";
     const prompt1 ={
         role: 'user',
         content: "How many sentences are there in '" + answer + "'? If one sentence, please write '1'."
@@ -35,12 +30,31 @@ async function validateOneTrigger( answer) {
         model: 'gpt-3.5-turbo-0301',
         messages: [prompt1]
     })
-    // const completion = await openai.createCompletion({
-    //     model : "gpt-3.5-turbo-0301",
-    //     prompt : prompt1
-    // });
-    //console.log(completion.data);
+    return (completion.data.choices[0].message.coxxczxcntent)
+}
+
+async function validateOne( answer) {
+    const prompt1 = {
+        role: 'user',
+        content: "How many triggers are there in '" + answer + "'? If one trigger, please write only '1'. If two or more triggers, please write every full trigger sentence."
+    };
+    const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo-0301',
+        messages: [prompt1]
+    })
     return (completion.data.choices[0].message.content)
+}
+async function validateAbstract( answer) {
+    const prompt1 = {
+        role: 'user',
+        content: "'" + answer + "' \n Is this topic abstract? If yes, please write '1'"
+    };
+    const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo-0301',
+        messages: [prompt1]
+    })
+    console.log(completion.data.choices[0].message.content);
+    return (completion.data.choices[0].message.content);
 }
 app.use(express.json())
 app.get('/', (req, res) => {
@@ -51,21 +65,23 @@ app.post('/sendTopic', async (req, res) => {
     const numberOfSentences = await validateOneSentence(answer)
     console.log(numberOfSentences)
     if(numberOfSentences != 1) {
-        //res.json({asking: "Topic", answer: "Please describe your problem clearly and concisely in 1 sentence. "})
-        //return;
         const returnAnswer = "Please describe your problem clearly and concisely in 1 sentence. ";
+        res.send(returnAnswer); return;
+    }
+    const isAbstract = await validateAbstract(answer);
+    console.log(isAbstract);
+    if(isAbstract[0] == '1'){
+        const returnAnswer = "Your description is too abstract. Please remember one particular experience when this problem happened, and tell me something about it.";
         res.send(returnAnswer); return;
     }
     const triggers = await validateOneTrigger(answer);
     console.log(triggers);
+    
     if(triggers[0] != '1'){
-        //res.json({asking: "Topic", answer: triggers + "\n Which is the worst part for you?"});
-        //return;
         const returnAnswer = triggers + "\n Which is the worst part for you?";
         res.send(returnAnswer); return;
     }
     const returnAnswer = "When you think '" + answer + "' what emotion do you feel right now? \n(Please use 1 word only)";
-    //res.json({asking: "Emotion", answer: "When you think '" + answer + "' what emotion do you feel right now? (Please use 1 word only)"}); return;
     res.send(returnAnswer); return;
 })
 app.listen(process.env.DEV_PORT, () => console.log('Express'));
